920e42ec717a676826ea3525160508a15cc0770e
leepears <1395772579@qq.com>
2017-11-06 22:42:27 +0800

    1.0上拉加载和下拉刷新
    Signed-off-by: leepears <1395772579@qq.com>

---
+++ refreshlibrary/src/main/java/com/example/refreshlibrary/SwipeRefreshLayoutOnRefresh.java
@@ -0,0 +1,22 @@
+package com.example.refreshlibrary;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+
+/**
+ *  Created by LEE on 2017/11/6.
+ */
+public class SwipeRefreshLayoutOnRefresh implements SwipeRefreshLayout.OnRefreshListener {
+    private PullLoadMoreRecyclerView mPullLoadMoreRecyclerView;
+
+    public SwipeRefreshLayoutOnRefresh(PullLoadMoreRecyclerView pullLoadMoreRecyclerView) {
+        this.mPullLoadMoreRecyclerView = pullLoadMoreRecyclerView;
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!mPullLoadMoreRecyclerView.isRefresh()) {
+            mPullLoadMoreRecyclerView.setIsRefresh(true);
+            mPullLoadMoreRecyclerView.refresh();
+        }
+    }
+}
